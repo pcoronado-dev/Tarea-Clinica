c46f43f766afecd3a6f42f9afc05ddfebe26967f
Piero Coronado <piero.coronado@tecsup.edu.pe>
2026-09-23 03:43:49 -0500

    Crear proyecto base de Clínica Salud+

---
+++ ClinicaSalud/app/src/main/java/com/coronado/clinica/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.coronado.clinica
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.coronado.clinica.ui.theme.ClinicaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MaterialTheme {
+                Surface {
+                    Inicio()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Inicio() {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            text = "Clínica Salud+",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Text(
+            text = "Reserva tu cita médica"
+        )
+    }
+}
