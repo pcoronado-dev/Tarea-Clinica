40aa394e4018a3e0e08ec231556617eabe3c53c1
Piero Coronado <piero.coronado@tecsup.edu.pe>
2026-09-23 03:47:31 -0500

    Se crea modelo y datos de médicos

--- ClinicaSalud/app/src/main/java/com/coronado/clinica/MainActivity.kt
+++ ClinicaSalud/app/src/main/java/com/coronado/clinica/MainActivity.kt
@@ -35,6 +35,30 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Inicio() {
 
+    val medicos = listOf(
+
+        Medico(
+            id = 1,
+            nombre = "Dr. Carlos Ramírez",
+            especialidad = "Cardiología",
+            calificacion = 4.8
+        ),
+
+        Medico(
+            id = 2,
+            nombre = "Dra. María López",
+            especialidad = "Pediatría",
+            calificacion = 4.9
+        ),
+
+        Medico(
+            id = 3,
+            nombre = "Dr. José Torres",
+            especialidad = "Dermatología",
+            calificacion = 4.7
+        )
+    )
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
---
+++ ClinicaSalud/app/src/main/java/com/coronado/clinica/Models.kt
@@ -0,0 +1,8 @@
+package com.coronado.clinica
+
+data class Medico(
+    val id: Int,
+    val nombre: String,
+    val especialidad: String,
+    val calificacion: Double
+)
